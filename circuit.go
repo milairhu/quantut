0bb0c162f72ae55838265a0336c032df975f321b
milairhu <hugo.milair@gmail.com>
2023-12-02 10:26:56 +0100

    Replace error by panic

--- circuit.go
+++ circuit.go
@@ -1,7 +1,6 @@
 package quantut
 
 import (
-	"errors"
 	"fmt"
 	"math/cmplx"
 	"math/rand"
@@ -44,16 +43,15 @@ func (c *QuantumCircuit) ClassicalRegister() []int {
 }
 
 // === Setters ===
-func (c *QuantumCircuit) SetQubit(numQubit int, comp1 complex128, comp2 complex128) error {
+func (c *QuantumCircuit) SetQubit(numQubit int, comp1 complex128, comp2 complex128) {
 	if numQubit >= c.numQubits || numQubit < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	if comp1*comp1+comp2*comp2 != 1 {
-		return errors.New("Qubit value must be normalized")
+		panic("Qubit value must be normalized")
 	}
 
 	c.qubitsValues[numQubit].Init(comp1, comp2)
-	return nil
 }
 
 // Définie le nombre de registres classics
@@ -63,93 +61,84 @@ func (c *QuantumCircuit) SetClassicalRegister(numRegister int) {
 
 // =============== Add gates to the circuit ===============
 // Hadmard
-func (c *QuantumCircuit) H(numQubit int) error {
+func (c *QuantumCircuit) H(numQubit int) {
 	if numQubit >= c.numQubits || numQubit < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: h, qubits: []int{numQubit}})
-	return nil
 }
 
 // Pauli X (NOT)
-func (c *QuantumCircuit) X(numQubit int) error {
+func (c *QuantumCircuit) X(numQubit int) {
 	if numQubit >= c.numQubits || numQubit < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: x, qubits: []int{numQubit}})
-	return nil
 }
 
 // Pauli Y
-func (c *QuantumCircuit) Y(numQubit int) error {
+func (c *QuantumCircuit) Y(numQubit int) {
 	if numQubit >= c.numQubits || numQubit < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: y, qubits: []int{numQubit}})
-	return nil
 }
 
 // Pauli Z
-func (c *QuantumCircuit) Z(numQubit int) error {
+func (c *QuantumCircuit) Z(numQubit int) {
 	if numQubit >= c.numQubits || numQubit < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: z, qubits: []int{numQubit}})
-	return nil
 }
 
 // CNOT
-func (c *QuantumCircuit) CNOT(control int, target int) error {
+func (c *QuantumCircuit) CNOT(control int, target int) {
 	if control >= c.numQubits || control < 0 || target >= c.numQubits || target < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: cnot, qubits: []int{control, target}})
-	return nil
 }
 
 // SWAP
-func (c *QuantumCircuit) SWAP(qubit1 int, qubit2 int) error {
+func (c *QuantumCircuit) SWAP(qubit1 int, qubit2 int) {
 	if qubit1 >= c.numQubits || qubit1 < 0 || qubit2 >= c.numQubits || qubit2 < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: swap, qubits: []int{qubit1, qubit2}})
-	return nil
 }
 
 // CCNOT
-func (c *QuantumCircuit) CCNOT(control1 int, control2 int, target int) error {
+func (c *QuantumCircuit) CCNOT(control1 int, control2 int, target int) {
 	if control1 >= c.numQubits || control1 < 0 || control2 >= c.numQubits || control2 < 0 || target >= c.numQubits || target < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: ccnot, qubits: []int{control1, control2, target}})
-	return nil
 }
 
 // CSWAP
-func (c *QuantumCircuit) CSWAP(control int, target1 int, target2 int) error {
+func (c *QuantumCircuit) CSWAP(control int, target1 int, target2 int) {
 	if control >= c.numQubits || control < 0 || target1 >= c.numQubits || target1 < 0 || target2 >= c.numQubits || target2 < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: cswap, qubits: []int{control, target1, target2}})
-	return nil
 }
 
 // TOFFOLI
-func (c *QuantumCircuit) TOFFOLI(control1 int, control2 int, target int) error {
+func (c *QuantumCircuit) TOFFOLI(control1 int, control2 int, target int) {
 	if control1 >= c.numQubits || control1 < 0 || control2 >= c.numQubits || control2 < 0 || target >= c.numQubits || target < 0 {
-		return errors.New("Qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	c.operations = append(c.operations, Operation{gate: toffoli, qubits: []int{control1, control2, target}})
-	return nil
 }
 
 // =============== Measure ===============
-func (c *QuantumCircuit) Measure(qubit int, register int) error {
+func (c *QuantumCircuit) Measure(qubit int, register int) {
 	if qubit >= c.numQubits || qubit < 0 {
-		return errors.New("qubit number out of range")
+		panic("Qubit number out of range")
 	}
 	if register >= len(c.classicalRegister) || register < 0 {
-		return errors.New("register number out of range")
+		panic("Register number out of range")
 	}
 	// The result of he measure is 0 or 1 depending on the value of the qubit.
 	// The result is stored in the classical register
@@ -169,5 +158,4 @@ func (c *QuantumCircuit) Measure(qubit int, register int) error {
 
 	//TODO : modifier le qubit mesuré pour qu'il soit projeté sur l'état mesuré
 
-	return nil
 }
--- qubit.go
+++ qubit.go
@@ -1,14 +1,11 @@
 package quantut
 
-import "errors"
-
 type Qubit [2]complex128 //Qubit is a slice of  complexes
 
-func (q *Qubit) Init(comp1 complex128, comp2 complex128) error {
+func (q *Qubit) Init(comp1 complex128, comp2 complex128) {
 	if comp1*comp1+comp2*comp2 != 1 {
-		return errors.New("values not normalized for initializing the qubit")
+		panic("Qubit value must be normalized")
 	}
 	q[0] = comp1
 	q[1] = comp2
-	return nil
 }
