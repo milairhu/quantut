f0720da6d19da4f71695996c5a0e9f4c879c7d9e
milairhu <hugo.milair@gmail.com>
2023-12-05 22:43:56 +0100

    Remove qasm files

--- main/main.go
+++ main/main.go
@@ -14,18 +14,23 @@ func main() {
 	circuit.H(0) //on applique une porte Hadamard sur le qubit 0
 	circuit.CNOT(1, 2)
 	circuit.CCNOT(0, 1, 2)
+	circuit.CSWAP(2, 0, 1)
+	circuit.Measure(2, 2)
 
 	circToCombine := quantut.NewQuantumCircuit(2) //on crée un circuit à 1 qubit
 	circToCombine.InitClassicalRegister(2)        //on crée un registre classique de 1 bit
-	circToCombine.CNOT(0, 1)                      //on applique une porte Hadamard sur le qubit 0
-	circToCombine.Measure(1, 1)                   //on mesure le qubit 0 et on stocke le résultat dans le registre classique 0
+	circToCombine.CNOT(0, 1)                      //on applique une porte CNOT sur le qubit 0
+	circToCombine.H(1)
+	circToCombine.X(1)
+	circToCombine.Y(1)
+	circToCombine.Z(1)
+	circToCombine.Measure(1, 1) //on mesure le qubit 0 et on stocke le résultat dans le registre classique 0
 
-	circuit = circuit.Compose(circToCombine) //on combine les deux circuits
-	circuit.SWAP(0, 1)
-	circuit.Measure(0, 0)       //on mesure le qubit 0 et on stocke le résultat dans le registre classique 0
-	circuit.Measure(1, 1)       //on mesure le qubit 1 et on stocke le résultat dans le registre classique 1
-	circuit.Measure(2, 2)       //on mesure le qubit 2 et on stocke le résultat dans le registre classique 2
-	circuit.ToQASM("test.qasm") //on écrit le circuit en QASM dans un fichier
+	circuit = circuit.Compose(circToCombine)    //on combine les deux circuits
+	circuit.Measure(0, 0)                       //on mesure le qubit 0 et on stocke le résultat dans le registre classique 0
+	circuit.Measure(1, 1)                       //on mesure le qubit 1 et on stocke le résultat dans le registre classique 1
+	circuit.Measure(2, 2)                       //on mesure le qubit 2 et on stocke le résultat dans le registre classique 2
+	circuit.ToQASM("test.qasm", "OPENQASM 2.0") //on écrit le circuit en QASM dans un fichier
 
 	simulator := quantut.NewSimulator(circuit, 1000)
 	res := simulator.Run()
