8f3432a667f4b83a63bbb2cb3ccbd69f0a534ef8
milairhu <hugo.milair@gmail.com>
2023-12-15 00:29:30 +0100

    Implement new way of applying gates

--- launchCircuit.go
+++ launchCircuit.go
@@ -1,83 +1,79 @@
 package quantut
 
-import (
-	"fmt"
-
-	"github.com/milairhu/quantut/utils"
-)
-
 // Simulate a circuit
 func (c *QuantumCircuit) LaunchCircuit() {
-	{
 
-		//Apply operations
-		for _, op := range c.operations {
-			if len(c.operations) == 0 {
-				panic("Operation is applied on no qubit")
+	/*var test []complex128 = []complex128{complex(1, 0), complex(0, 1), complex(0, 0), complex(0, 0)}
+	fmt.Println(test)
+	fmt.Println("X : ", xeffect(1, test, 2))
+	fmt.Println("Y : ", yeffect(1, test, 2))
+	fmt.Println("Z : ", zeffect(1, test, 2))
+	fmt.Println("H : ", heffect(1, test, 2))
+
+	fmt.Println("CNOT : ", cnoteffect(0, 1, test, 2))
+	fmt.Println("CNOT : ", cnoteffect(1, 0, test, 2))
+
+	fmt.Println("SWAP : ", swapeffect(0, 1, test, 2))
+	fmt.Println("SWAP : ", swapeffect(1, 0, test, 2))
+
+	test2 := []complex128{complex(1, 0), complex(0, 0), complex(0, 0), complex(1, 0),
+		complex(0, 0), complex(0, 0), complex(1, 0), complex(0, 0)}
+
+	fmt.Println("CCNOT : ", ccnoteffect(0, 1, 2, test2, 3))
+	fmt.Println("CCNOT : ", ccnoteffect(1, 0, 2, test2, 3))
+	fmt.Println("CCNOT : ", ccnoteffect(0, 2, 1, test2, 3))
+
+	fmt.Println("CSWAP : ", cswapeffect(0, 1, 2, test2, 3))
+	fmt.Println("CSWAP : ", cswapeffect(1, 0, 2, test2, 3))
+	fmt.Println("CSWAP : ", cswapeffect(0, 2, 1, test2, 3))
+	*/
+	//Apply operations
+	for _, op := range c.operations {
+		if len(c.operations) == 0 {
+			panic("Operation is applied on no qubit")
+		}
+
+		// Particular cases : measure and initialization
+		switch op.Gate().Id() {
+		case "MEASURE":
+			if len(op.Qubits()) != 2 {
+				panic("Measure operation must have 1 qubit and 1 classical register")
 			}
+			//Measure the qubit in the classical register
+			measureEffect(c, op.Qubits()[0], op.Qubits()[1])
 
-			// Particular cases : measure and initialization
+		case "INIT":
+			if len(op.Qubits()) != 1 {
+				panic("Initialization operation must have 1 qubit")
+			}
+			if len(op.Options()) != 2 {
+				panic("Initialization operation must have 2 values for the qubit")
+			}
+			//Initialize the qubit
+			c.SetQubit(op.Qubits()[0], op.Options()[0], op.Options()[1])
+
+		default:
+			//For any other operation than measure and initialization
 			switch op.Gate().Id() {
-			case "MEASURE":
-				if len(op.Qubits()) != 2 {
-					panic("Measure operation must have 1 qubit and 1 classical register")
-				}
-				//Measure the qubit in the classical register
-				measureEffect(c, op.Qubits()[0], op.Qubits()[1])
-
-			case "INIT":
-				if len(op.Qubits()) != 1 {
-					panic("Initialization operation must have 1 qubit")
-				}
-				if len(op.Options()) != 2 {
-					panic("Initialization operation must have 2 values for the qubit")
-				}
-				//Initialize the qubit
-				c.SetQubit(op.Qubits()[0], op.Options()[0], op.Options()[1])
+			case "X":
+				c.globalState = xeffect(op.Qubits()[0], c.globalState, c.NumQubits())
+			case "Y":
+				c.globalState = yeffect(op.Qubits()[0], c.globalState, c.NumQubits())
+			case "Z":
+				c.globalState = zeffect(op.Qubits()[0], c.globalState, c.NumQubits())
+			case "H":
+				c.globalState = heffect(op.Qubits()[0], c.globalState, c.NumQubits())
+			case "CNOT":
+				c.globalState = cnoteffect(op.Qubits()[0], op.Qubits()[1], c.globalState, c.NumQubits())
+			case "SWAP":
+				c.globalState = swapeffect(op.Qubits()[0], op.Qubits()[1], c.globalState, c.NumQubits())
+			case "CCNOT":
+				c.globalState = ccnoteffect(op.Qubits()[0], op.Qubits()[1], op.Qubits()[2], c.globalState, c.NumQubits())
+			case "CSWAP":
+				c.globalState = cswapeffect(op.Qubits()[0], op.Qubits()[1], op.Qubits()[2], c.globalState, c.NumQubits())
 
 			default:
-				//For any other operation than measure and initialization
-				var nbQubits int = len(op.Qubits())
-
-				if nbQubits == 1 {
-					//If only one qubit is involved, apply the gate
-					qubitToVector := c.qubitsValues[numQubit].Vector()
-					var calc = utils.ComplexMatrixProduct(op.Gate().Effect(), qubitToVector)
-					c.SetQubit(numQubit, calc[0][0], calc[1][0])
-				} else {
-					// If more than one qubit is involved, qubit must be synchronized with the others
-					// Get the other qubits involved in the operation
-					controlQubits := make([]int, 0)
-					for i := 0; i < int(op.Gate().NbControlQubit()); i++ {
-						controlQubits = append(controlQubits, op.Qubits()[i])
-					}
-					targetQubits := make([]int, 0)
-					for i := int(op.Gate().NbControlQubit()); i < len(op.Qubits()); i++ {
-						targetQubits = append(targetQubits, op.Qubits()[i])
-					}
-					//Convention : control qubits are given first in the operation
-					//Case 1 : the qubit is a control qubit
-					if op.IsControlQubit(numQubit) {
-						//Qubit sends its value to the target qubit(s) //TODO : checker CSWAP, 2 cibles
-						for _, q := range targetQubits {
-							qubitChanMap[q] <- c.qubitsValues[numQubit]
-							fmt.Println("Qubit", numQubit, "sent value to qubit", q)
-						}
-					} else { //Case 2 : the qubit is a target qubit
-						//TODO voir comment on fait pour réaliser le calcul quand on a plusieurs cible : on désigne un qubit qui fait le calcul et qui envoie le résultat aux autres ?
-						//Qubit waits for the control qubit(s) to send its value
-						receivedValues := make([]Qubit, op.gate.nbControlQubit)
-						for i, q := range controlQubits {
-							receivedValues[i] = <-qubitChanMap[q] //TODO vérifier qu'on n'a pas besoin de recevoir les valeurs dans l'ordre (ou de les reranger)
-							fmt.Println("Qubit", numQubit, "received value from qubit", q)
-						}
-
-						//Apply the gate
-						//TODO
-						fmt.Println("Qubit", numQubit, "received values", receivedValues)
-					}
-				}
-
+				panic("Unknown gate" + op.Gate().Id())
 			}
 		}
 	}
